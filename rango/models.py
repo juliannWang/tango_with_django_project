347db4e2e5e64e99599bebbaf046467b3af63aab
Julian Wang <2613319w@student.gla.ac.uk>
2023-02-06 17:39:49 +0000

    chapter 9 completed all tests passed

--- rango/models.py
+++ rango/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique= True)
@@ -25,3 +26,15 @@ class Page(models.Model):
     
     def __str__(self):
         return self.title
+
+class UserProfile(models.Model):
+    #required line, links userprofile to a user model instance basically like a foreign key
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+    #additional attributes
+    website = models.URLField(blank=True)
+    picture = (models.ImageField(upload_to='profile_images',blank = True))
+    
+    def __str__(self):
+        return self.user.username
+    
